5afafb5c9d3b63565e892ad0f8d2ad27206729cc
msizanoen1 <qtmlabs@protonmail.com>
2021-02-15 18:39:19 +0700

    [cursed_linux] unlock drives when done

--- src/partition/repart.c
+++ src/partition/repart.c
@@ -1774,6 +1774,8 @@ static void context_unload_partition_table(Context *context) {
         context->total = UINT64_MAX;
 
         if (context->fdisk_context) {
+                if (flock(fdisk_get_devfd(context->fdisk_context), LOCK_UN) < 0)
+                        log_error_errno(errno, "Failed to unlock block device: %m");
                 fdisk_unref_context(context->fdisk_context);
                 context->fdisk_context = NULL;
         }
